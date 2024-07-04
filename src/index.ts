c1f64b12d8fcb2dfa97d379cb87ac444348dffcf
Kartikhub <sharmakartik66@gmail.com>
2024-07-04 19:59:12 +0530

    Updated Unsubscribe logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { WebSocketServer, WebSocket } from 'ws';
+import { createClient } from 'redis';
+ 
 
 const wss = new WebSocketServer({ port: 8080 });
+
+const publisherClient = createClient();
+const subscriberClient = createClient();
+
+startConnect(); 
 
 const usersMap: Map<number, {
     ws: WebSocket,
@@ -11,15 +18,15 @@ setInterval(() => {
     console.log(usersMap);
 }, 5000);
 
+
 wss.on('connection', function connection(userSocket) {
     userSocket.on('error', console.error);
     const id = Math.random();
 
-    userSocket.on('message', function message(data) {
+    userSocket.on('message', async function message(data) {
         const parsedData = JSON.parse(data as unknown as string);
         if (parsedData.type === "SUBSCRIBE") {
             let user = usersMap.get(id);
-
             if(!user) {
                 usersMap.set(id, {
                     ws: userSocket,
@@ -32,6 +39,11 @@ wss.on('connection', function connection(userSocket) {
                 }
                 usersMap.set(id, user);
             }
+            await subscriberClient.subscribe(parsedData.roomId, (message) => {
+                    if(user && user.rooms.includes(parsedData.roomId)) {
+                        user.ws.send(message);
+                    }
+            });
         }
         if (parsedData.type === "UNSUBSCRIBE") {
             const roomId = parsedData.roomId;
@@ -39,16 +51,19 @@ wss.on('connection', function connection(userSocket) {
             if(user && user.rooms.includes(roomId)) {
                 user.rooms = user.rooms.filter(x => x !== parsedData.roomId);
             }
+            subscriberClient.unsubscribe(parsedData.roomId, () => {console.log("Unsubscribed")});
         }
         if (parsedData.type === "SENDMESSAGE") {
             const message = parsedData.message;
             const roomId = parsedData.roomId;
-            usersMap.forEach((user) => {
-                const { ws, rooms } = user;
-                if(rooms.includes(roomId)){
-                    ws.send(message);
-                }
-            });
+            await publisherClient.publish(roomId, message);
         }
     });
 });
+
+
+
+async function startConnect() {
+    await publisherClient.connect();
+    await subscriberClient.connect();
+}
